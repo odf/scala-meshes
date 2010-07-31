5bef3b82f297cccc4788da8a1f6a7e4f14ea666c
odf <olaf.delgado@gmail.com>
2010-07-31 22:21:26 +1000

    The Split command now only unwelds groups if given an output file name.

--- src/org/gavrog/joss/meshes/Split.scala
+++ src/org/gavrog/joss/meshes/Split.scala
@@ -1,5 +1,5 @@
 /*
-   Copyright 2009 Olaf Delgado-Friedrichs
+   Copyright 2010 Olaf Delgado-Friedrichs
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
@@ -19,11 +19,43 @@ package org.gavrog.joss.meshes
 
 import java.io.FileWriter
 import scala.io.Source
+import scala.collection.mutable.ArrayBuffer
 
 object Split {
-  def main(args : Array[String]) : Unit =
-    for (m <- new Mesh(Source.fromFile(args(0))).splitByGroup) {
-      val name = m.groups.head.name
-      m.write(new FileWriter("%s.obj" format name), name)
+  def main(args : Array[String]) {
+	if (args.size > 1) {
+	  val m = join(new Mesh(Source.fromFile(args(0))).splitByGroup)
+	  m.write(new FileWriter(args(1)), null)	}
+	else {
+	  for (m <- new Mesh(Source.fromFile(args(0))).splitByGroup) {
+		val name = m.groups.head.name
+		m.write(new FileWriter("%s.obj" format name), name)
+	  }
+	}
+  }
+  
+  def join(parts: Iterable[Mesh]) = {
+	val m = new Mesh()
+	var vMap = Map[(Mesh, Int), Int]()
+	var tMap = Map[(Mesh, Int), Int]()
+	var nMap = Map[(Mesh, Int), Int]()
+    for (part <- parts if part.numberOfFaces > 0) {
+      vMap ++= part.vertices.map(v => ((part, v.nr), m.addVertex(v.pos).nr))
+      tMap ++= part.textureVertices.map(t =>
+      	((part, t.nr), m.addTextureVertex(t.pos).nr))
+      nMap ++= part.normals.map(n => ((part, n.nr), m.addNormal(n.value).nr))
     }
+	for (part <- parts; f <- part.faces) {
+	  val cs = ArrayBuffer() ++ f.vertexChambers
+	  val vs = cs.map(c => vMap((part, c.vertexNr)))
+	  val vt = cs.map(c => tMap((part, c.tVertexNr)))
+	  val vn = cs.map(c => nMap((part, c.normalNr)))
+	  val g = m.addFace(vs, vt, vn)
+	  g.material = m.material(f.material.name)
+	  g.obj = m.obj(f.obj.name)
+	  g.group = m.group(f.group.name)
+	  g.smoothingGroup = f.smoothingGroup
+	}
+	m
+  }
 }
